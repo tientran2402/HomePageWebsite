fa3fdd68c68e8d1b74d8acd65b67adb19302ba3a
Tran Tien <tientran163255@gmail.com>
2024-03-28 09:03:56 +0700

    HomePage

---
+++ script.js
@@ -0,0 +1,44 @@
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const slides = document.querySelectorAll('.slide');
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+// slider next button
+nextBtn.onclick = () => {
+    slides.forEach((slide) => {     
+        slide.classList.remove('active');
+    })
+
+    slideNumber++;
+
+    if (slideNumber > (numberOfSlides - 1)) 
+    {
+        slideNumber = 0;
+    }
+    slides[slideNumber].classList.add('active');
+}
+
+
+//slider prev button
+// slider next button
+prevBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    })
+
+    slideNumber--;
+
+    if (slideNumber < 0 ) 
+    {
+        slideNumber = numberOfSlides - 1;
+    }
+    slides[slideNumber].classList.add('active');
+}
+
+    console.log(slides.length)
+
+
+
+
+
